334cb89cc9e819179d5262d526487228e24fea83
kdev76979-mac <kdev76979@gmail.com>
2026-08-09 23:45:27 +0530

    Add solution v1 for Backspace String Compare (java)

---
+++ backspace-string-compare/solutions/solution_1_2026-08-09.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+
+        while (i >= 0 || j >= 0) {
+            i = getNextValidCharIndex(s, i);
+            j = getNextValidCharIndex(t, j);
+
+            if (i < 0 && j < 0) return true;
+            if (i < 0 || j < 0) return false;
+
+            if (s.charAt(i) != t.charAt(j)) return false;
+
+            i--;
+            j--;
+        }
+        return true;
+    }
+
+    private int getNextValidCharIndex(String str, int index) {
+        int backspace = 0;
+
+        while (index >= 0) {
+            if (str.charAt(index) == '#') {
+                backspace++;
+            } else if (backspace > 0) {
+                backspace--;
+            } else {
+                break;
+            }
+            index--;
+        }
+        return index;
+        
+    }
+}
